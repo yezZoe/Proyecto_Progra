260fd7a7bf54f32ea7a27519e36479967986c257
YESLYDANIELAFIGUEROA <YESLYDANIELAFIGUEROA@Yesly>
2024-11-19 15:54:03 -0600

    hice lo del idioma en mi clase natacion y lo conecte con el main para mostrarlo en consola

--- proyecto/src/Espacios/AreaNatacion.java
+++ proyecto/src/Espacios/AreaNatacion.java
@@ -1,17 +1,13 @@
 package Espacios;
 
-
 import java.time.LocalTime;
 import java.util.List;
 import java.util.ArrayList;
 
-
 /**
  * Clase que representa un área de natación, derivada de EspacioDeportivo.
- * 
+ *
  */
-
-
 public class AreaNatacion extends EspacioDeportivo {
 
     private LocalTime horarioApertura;
@@ -20,6 +16,7 @@ public class AreaNatacion extends EspacioDeportivo {
     private double temperaturaAgua;
     private double profundidad;
     private boolean servicioMantenimiento;
+    SistemaReserva sistema = new SistemaReserva();
 
     /**
      * Constructor para inicializar un área de natación con nombre, tipo,
@@ -33,27 +30,32 @@ public class AreaNatacion extends EspacioDeportivo {
      * @param horarioApertura Hora de apertura del área de natación.
      * @param horarioCierre Hora de cierre del área de natación.
      * @param capacidadNatacion Capacidad específica para el área de natación.
-     * @param temperaturaAgua Temperatura del agua del área de natación.
      * @param profundidad Profundidad máxima del área de natación.
      * @param servicioMantenimiento Indica si el área de natación tiene servicio
      * de mantenimiento.
      */
     public AreaNatacion(String nombreEspacio, String tipoEspacio, int capacidadEspacio, int idEspacio, String ubicacionEspacio,
-            LocalTime horarioApertura, LocalTime horarioCierre, int capacidadNatacion, double temperaturaAgua,
+            LocalTime horarioApertura, LocalTime horarioCierre, int capacidadNatacion,
             double profundidad, boolean servicioMantenimiento) {
         super(nombreEspacio, tipoEspacio, capacidadEspacio);
         this.horarioApertura = horarioApertura;
         this.horarioCierre = horarioCierre;
         this.capacidadNatacion = capacidadNatacion;
-        this.temperaturaAgua = temperaturaAgua;
         this.profundidad = profundidad;
         this.servicioMantenimiento = servicioMantenimiento;
     }
 
     
+
+    
+
     public void ajustarTemperatura(double nuevaTemperatura) {
         this.temperaturaAgua = nuevaTemperatura;
-        System.out.println("La temperatura del agua se ha ajustado a: " + nuevaTemperatura + "°C");
+        System.out.println(sistema.translate(
+                "Water temperature adjusted to: " + nuevaTemperatura + "°C.",
+                "Temperatura del agua ajustada a: " + nuevaTemperatura + "°C.",
+                "Temperatura da água ajustada para: " + nuevaTemperatura + "°C."
+        ));
     }
 
     /**
@@ -61,27 +63,28 @@ public class AreaNatacion extends EspacioDeportivo {
      *
      * @return Lista de servicios adicionales.
      */
-    public List<String> serviciosAdicionalesGym() {
-        List<String> servicios = new ArrayList<>();
-        servicios.add("Sauna");
-        servicios.add("Jacuzzi");
-        servicios.add("Entrenador personal");
-        return servicios;
+    public void mostrarServiciosAdicionalesNAT() {
+        System.out.println(sistema.translate(
+                "Additional swimming area services:",
+                "Servicios adicionales del area de natacion:",
+                "Serviços adicionais do área de natação:"
+        ));
+        List<String> servicios = serviciosAdicionalesNAT();
+        for (String servicio : servicios) {
+            System.out.println("- " + servicio);
+        }
     }
 
-    
+
     public void mostrarProfundidad() {
-        System.out.println("La profundidad máxima del área de natación es: " + profundidad + " metros.");
+        System.out.println(sistema.translate(
+            "The maximum depth of the swimming area is: 30 meters.",
+            "La profundidad máxima del área de natación es: 30 metros.",
+            "A profundidade máxima da área de natação é: 30 metros."
+        ));
     }
-
-    /**
-     * Muestra la disponibilidad del área de natación con información adicional.
-     * @return 
-     */
-
     
-
-   
+    
     public LocalTime getHorarioApertura() {
         return horarioApertura;
     }
@@ -128,5 +131,13 @@ public class AreaNatacion extends EspacioDeportivo {
 
     public void setServicioMantenimiento(boolean servicioMantenimiento) {
         this.servicioMantenimiento = servicioMantenimiento;
+    }
+
+    public List<String> serviciosAdicionalesNAT() {
+        List<String> servicios = new ArrayList<>();
+        servicios.add(sistema.translate("1.Sauna", "1.Sauna", "1.Sauna"));
+        servicios.add(sistema.translate("2.Jacuzzi", "2.Jacuzzi", "2.Jacuzzi"));
+        servicios.add(sistema.translate("3.Personal Trainer", "3.Entrenador personal", "3.Treinador pessoal"));
+        return servicios;
     }
 }
--- proyecto/src/Espacios/SistemaReserva.java
+++ proyecto/src/Espacios/SistemaReserva.java
@@ -18,40 +18,50 @@ public class SistemaReserva {
     private List<EspacioDeportivo> listaEspaciosDeportivos = new ArrayList<>();
     private List<Usuario> listaUsuarios = new ArrayList<>();
     private int idReservaActual = 1;
-    private String language = "EN";
-    
+    public static String language = "EN";
 
     public SistemaReserva() {
 
-        listaEspaciosDeportivos.add(new EspacioDeportivo("Piscina Olímpica", "Natación", 50));
-        listaEspaciosDeportivos.add(new EspacioDeportivo("Cancha de Fútbol", "Fútbol", 30));
-        listaEspaciosDeportivos.add(new EspacioDeportivo("Gimnasio Principal", "Gimnasia", 20));
+        listaEspaciosDeportivos.add(new EspacioDeportivo(
+                "Piscina Olímpica",
+                "Natación", 50));
+        listaEspaciosDeportivos.add(new EspacioDeportivo(
+                "Cancha de Fútbol",
+                "Fútbol", 30));
+        listaEspaciosDeportivos.add(new EspacioDeportivo(
+                "Gimnasio Principal",
+                "Gimnasia", 20));
     }
-    
-    
-    private String translate(String en, String es, String pt){
+
+    public static String translate(String en, String es, String pt) {
         switch (language) {
             case "ES":
-              return es;
+                return es;
             case "Pt":
-              return pt;
-              
+                return pt;
+
             default:
-             return en;
-             
+                return en;
+
         }
     }
+
     public void registrarUsuario() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(translate(
-        "Select user type",
-        "seleccione el tipo de usuario",
-        " Selecione o tipo de usuario:"));
-       
-        System.out.println("1." + translate("Client", "Cliente"," Cliente"));
-        System.out.println("2." + translate("Administrator","Administrador","Administrator"));
-        
-        
+                "Select user type",
+                "seleccione el tipo de usuario",
+                " Selecione o tipo de usuario:"));
+
+        System.out.println("1." + translate(
+                "Client",
+                "Cliente",
+                " Cliente"));
+        System.out.println("2." + translate(
+                "Administrator",
+                "Administrador",
+                "Administrator"));
+
         int tipoUsuario = scanner.nextInt();
         scanner.nextLine();
 
@@ -80,9 +90,10 @@ public class SistemaReserva {
                     "Ingrese su número de carnet: ",
                     "Digite seu número de cartão: "));
             String numeroCarnet = scanner.nextLine();
-            UsuarioAdministrador admin = new UsuarioAdministrador(nombre, identificacion, numeroCarnet);
+            UsuarioAdministrador admin = new UsuarioAdministrador(nombre,
+                    identificacion, numeroCarnet);
             listaUsuarios.add(admin);
-           System.out.println(translate(
+            System.out.println(translate(
                     "Administrator successfully registered.",
                     "Administrador registrado exitosamente.",
                     "Administrador registrado com sucesso."));
@@ -96,7 +107,7 @@ public class SistemaReserva {
     }
 
     public void mostrarUsuarios() {
-       System.out.println(translate(
+        System.out.println(translate(
                 "Available sports spaces:",
                 "Espacios deportivos disponibles:",
                 "Espaços desportivos disponíveis:"));
@@ -108,90 +119,130 @@ public class SistemaReserva {
     public void crearReserva() {
         Scanner scanner = new Scanner(System.in);
 
-         System.out.println(translate(
+        System.out.println(translate(
                 "Welcome to the Reservation System",
                 "Bienvenido al Sistema de Reservas",
                 "Bem-vindo ao Sistema de Reservas"));
         mostrarEspaciosDisponibles();
-      System.out.print(translate(
+
+        System.out.print(translate(
                 "Select the number of the sports space you want to reserve: ",
                 "Seleccione el número del espacio deportivo que desea reservar: ",
                 "Selecione o número do espaço desportivo que deseja reservar: "));
         int opcionEspacio = scanner.nextInt();
-        scanner.nextLine();
+        EspacioDeportivo espacioSeleccionado = listaEspaciosDeportivos.get(opcionEspacio - 1);
+        System.out.println(translate(
+                "You selected: ",
+                "Ha seleccionado: ",
+                "Você selecionou: ") + espacioSeleccionado.getNombreEspacio());
+
+        if (opcionEspacio == 1) {
+            AreaNatacion temp = new AreaNatacion(language,
+                    language, opcionEspacio,
+                    opcionEspacio, language,
+                    LocalTime.MIDNIGHT,
+                    LocalTime.MIDNIGHT,
+                    opcionEspacio,
+                    opcionEspacio, true);
+
+            temp.mostrarProfundidad();
+            
+            System.out.println(translate(
+                    "Enter the desired water temperature",
+                    "Ingrese la temperatura deseada del agua",
+                    "Insira a temperatura da água desejada"));
+            double temperature = scanner.nextDouble();
+
+            temp.ajustarTemperatura(temperature);
+
+            temp.mostrarServiciosAdicionalesNAT();
+            System.out.println(translate(
+                    "- 0.I do not want any additional service",
+                    "- 0.No deseo ningun servicio adicional",
+                    "- 0.Não quero nenhum serviço adicional"));
+
+            System.out.println(translate(
+                    "Select an additional service for the swimming area:",
+                    "Seleccione un servicio adicional para el area de natacion: ",
+                    "Selecione um serviço adicional para a área de natação:"));
+
+            int op1 = scanner.nextInt();
+            scanner.nextLine();
+
+            if (op1 != 0) {
+                System.out.println("Servicio seleccionado: ");
+            }
+        }
 
         if (opcionEspacio < 1 || opcionEspacio > listaEspaciosDeportivos.size()) {
-           System.out.println(translate(
+            System.out.println(translate(
                     "Invalid option.",
                     "Opción inválida.",
                     "Opção inválida."));
             return;
         }
 
-        EspacioDeportivo espacioSeleccionado = listaEspaciosDeportivos.get(opcionEspacio - 1);
-       System.out.println(translate(
-                "You selected: ",
-                "Ha seleccionado: ",
-                "Você selecionou: ") + espacioSeleccionado.getNombreEspacio());
-
         System.out.print(translate(
                 "Enter the reservation name: ",
                 "Ingrese el nombre de la reserva: ",
                 "Digite o nome da reserva: "));
         String nombreReserva = scanner.nextLine();
-       
 
         try {
             SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy/MM/dd");
             dateFormatter.setLenient(false); // Asegura validación estricta
 
-           
             System.out.print(translate(
                     "Enter the start date (yyyy/MM/dd): ",
                     "Ingrese la fecha de inicio (yyyy/MM/dd): ",
                     "Digite a data de início (yyyy/MM/dd): "));
             Date fechaInicio = dateFormatter.parse(scanner.nextLine());
-            
-      
+
             System.out.print(translate(
                     "Enter the end date (yyyy/MM/dd): ",
                     "Ingrese la fecha de fin (yyyy/MM/dd): ",
                     "Digite a data final (yyyy/MM/dd): "));;
             Date fechaFin = dateFormatter.parse(scanner.nextLine());
-            
-            System.out.println("Fecha de Inicio: " + dateFormatter.format(fechaInicio));
-        System.out.println("Fecha que Finaliza: " + dateFormatter.format(fechaFin));
-            
-            DateTimeFormatter horaFormatter = DateTimeFormatter.ofPattern("HH:mm");
-            
+
+            System.out.println("Fecha de Inicio: "
+                    + dateFormatter.format(fechaInicio));
+            System.out.println("Fecha que Finaliza: "
+                    + dateFormatter.format(fechaFin));
+
+            DateTimeFormatter horaFormatter
+                    = DateTimeFormatter.ofPattern("HH:mm");
+
             // Solicita la hora de inicio
-                 System.out.print(translate(
+            System.out.print(translate(
                     "Enter the start time (HH:mm, 24-hour format): ",
                     "Ingrese la hora de inicio (HH:mm, formato militar): ",
                     "Digite a hora de início (HH:mm, formato 24 horas): "));
-            String horaInicioStr = scanner.nextLine().trim(); // Elimina espacios adicionales
-            System.out.println("Hora inicio ingresada: " + horaInicioStr); // Debug
-            LocalTime horaInicio = LocalTime.parse(horaInicioStr, horaFormatter);
+            String horaInicioStr = scanner.nextLine().trim();
+            System.out.println("Hora inicio ingresada: " + horaInicioStr);
+            LocalTime horaInicio = LocalTime.parse(horaInicioStr,
+                    horaFormatter);
 
             // Solicita la hora de fin
-             System.out.print(translate(
+            System.out.print(translate(
                     "Enter the end time (HH:mm, 24-hour format): ",
                     "Ingrese la hora de fin (HH:mm, formato militar): ",
                     "Digite a hora final (HH:mm, formato 24 horas): "));
-            String horaFinStr = scanner.nextLine().trim(); // Elimina espacios adicionales
-            System.out.println("Hora fin ingresada: " + horaFinStr); // Debug
-            LocalTime horaFin = LocalTime.parse(horaFinStr, horaFormatter);
+            String horaFinStr = scanner.nextLine().trim();
+            System.out.println("Hora fin ingresada: " + horaFinStr);
+            LocalTime horaFin = LocalTime.parse(horaFinStr,
+                    horaFormatter);
 
             Reserva nuevaReserva = new Reserva(idReservaActual++, nombreReserva,
                     fechaInicio, fechaFin, horaInicio, horaFin, true);
             listaReservas.add(nuevaReserva);
 
-             System.out.println(translate(
+            System.out.println(translate(
                     "Reservation created successfully.",
                     "Reserva creada con éxito.",
                     "Reserva criada com sucesso."));
-            System.out.println(translate("Reservation details: ", 
-                    "Detalles de la reserva: ", "Detalhes da reserva: ") + "\n" 
+            System.out.println(translate("Reservation details: ",
+                    "Detalles de la reserva: ", "Detalhes da reserva: ")
+                    + "\n"
                     + nuevaReserva);
         } catch (ParseException e) {
             System.out.println(translate(
@@ -221,52 +272,75 @@ public class SistemaReserva {
     }
 
     public void mostrarEspaciosDisponibles() {
-       System.out.println(translate(
+        System.out.println(translate(
                 "Available sports spaces:",
                 "Espacios deportivos disponibles:",
                 "Espaços desportivos disponíveis:"));
         for (int i = 0; i < listaEspaciosDeportivos.size(); i++) {
             EspacioDeportivo espacio = listaEspaciosDeportivos.get(i);
-            System.out.println((i + 1) + ". " + espacio.getNombreEspacio() 
-                    + " - " + translate("Type", "Tipo", "Tipo") + ": " +
-                    espacio.getTipoEspacio() + ", " 
-                    + translate("Capacity", "Capacidad", "Capacidade") + ": " 
+            System.out.println((i + 1) + ". " + espacio.getNombreEspacio()
+                    + " - " + translate("Type", "Tipo", "Tipo") + ": "
+                    + espacio.getTipoEspacio() + ", "
+                    + translate("Capacity", "Capacidad",
+                            "Capacidade") + ": "
                     + espacio.getCapacidad());
         }
     }
 
     public static void main(String[] args) {
-      
+
         Scanner scanner = new Scanner(System.in);
-        
-         System.out.println("Select your language:");
+
+        System.out.println("Select your language:");
         System.out.println("Seleccione su idioma:");
         System.out.println("Selecione seu idioma:");
         System.out.println("1. English");
         System.out.println("2. Español");
         System.out.println("3. Português");
-        
-        
+
+        int op = scanner.nextInt();
+        switch (op) {
+
+            case 1:
+                language = "EN";
+                break;
+            case 2:
+                language = "ES";
+                break;
+            case 3:
+                language = "Pt";
+                break;
+            default:
+                throw new AssertionError();
+        }
+
         SistemaReserva sistemaReserva = new SistemaReserva();
-        
-        System.out.println("Bienvenido al Sistema de Reservas");
+
+        System.out.println(translate("Welcome to the Reservation System",
+                "Bienvenido al Sistema de Reservas",
+                "Bem-vindo ao Sistema de Reservas"));
 
         boolean continuar = true;
         while (continuar) {
-            
+
             System.out.println(sistemaReserva.translate(
                     "\nSelect an option:",
                     "\nSeleccione una opción:",
                     "\nSelecione uma opção:"));
-            System.out.println("1. " + sistemaReserva.translate("Register User", 
+            System.out.println("1. " + sistemaReserva.translate(
+                    "Register User",
                     "Registrar Usuario", "Registrar Usuário"));
-            System.out.println("2. " + sistemaReserva.translate("Create Reservation",
+            System.out.println("2. " + sistemaReserva.translate(
+                    "Create Reservation",
                     "Crear Reserva", "Criar Reserva"));
-            System.out.println("3. " + sistemaReserva.translate("Show Users", 
+            System.out.println("3. " + sistemaReserva.translate(
+                    "Show Users",
                     "Mostrar Usuarios", "Mostrar Usuários"));
-            System.out.println("4. " + sistemaReserva.translate("Show Reservations"
-                    , "Mostrar Reservas", "Mostrar Reservas"));
-            System.out.println("5. " + sistemaReserva.translate("Exit", "Salir",
+            System.out.println("4. " + sistemaReserva.translate(
+                    "Show Reservations",
+                    "Mostrar Reservas", "Mostrar Reservas"));
+            System.out.println("5. " + sistemaReserva.translate(
+                    "Exit", "Salir",
                     "Sair"));
 
             int opcion = scanner.nextInt();
@@ -286,13 +360,13 @@ public class SistemaReserva {
                     break;
                 case 5:
                     continuar = false;
-                     System.out.println(sistemaReserva.translate(
+                    System.out.println(sistemaReserva.translate(
                             "Exiting the system...",
                             "Saliendo del sistema...",
                             "Saindo do sistema..."));
                     break;
                 default:
-                   System.out.println(sistemaReserva.translate(
+                    System.out.println(sistemaReserva.translate(
                             "Invalid option. Try again.",
                             "Opción inválida. Intente nuevamente.",
                             "Opção inválida. Tente novamente."));

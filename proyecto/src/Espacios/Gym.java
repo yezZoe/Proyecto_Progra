fbb81ca0beba3b7542ceff68db0241a1315b36f3
YESLYDANIELAFIGUEROA <YESLYDANIELAFIGUEROA@Yesly>
2024-11-19 19:27:02 -0600

    arregle lo del jacuzzi en gym y puse eventos sociales

--- proyecto/src/Espacios/Gym.java
+++ proyecto/src/Espacios/Gym.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class Gym extends EspacioDeportivo {
 
-    private  String horarioApertura;
+    private String horarioApertura;
     private String horarioCierre;
-    private  int capacidadMaxima;
+    private int capacidadMaxima;
     private int cantidadMaquinas;
     private ArrayList<String> serviciosAdicionales;
     private final SistemaReserva sistema;
@@ -25,52 +25,68 @@ public class Gym extends EspacioDeportivo {
      * @param cantidadMaquinas Cantidad de máquinas disponibles.
      * @param language
      */
-    public Gym(String nombreEspacio, String tipoEspacio, int capacidadEspacio, int idEspacio,
-               String ubicacionEspacio, String horarioApertura, int capacidadMaxima,
-               int cantidadMaquinas, String horarioCierre, String language) {
-       super(nombreEspacio, tipoEspacio, capacidadEspacio);
-       
+    public Gym(String nombreEspacio, String tipoEspacio, int capacidadEspacio,
+            int idEspacio,
+            String ubicacionEspacio, String horarioApertura,
+            int capacidadMaxima,
+            int cantidadMaquinas, String horarioCierre, String language) {
+        super(nombreEspacio, tipoEspacio, capacidadEspacio);
+
         this.horarioApertura = horarioApertura;
         this.capacidadMaxima = capacidadMaxima;
         this.cantidadMaquinas = cantidadMaquinas;
         this.horarioCierre = horarioCierre;
         this.serviciosAdicionales = new ArrayList<>();
-        this.sistema = new SistemaReserva();  
-    
-      // Agregar servicios adicionales traducidos
-        serviciosAdicionales.add(sistema.translate("2.Jacuzzi", "2.Jacuzzi", "2.Jacuzzi"));
-        serviciosAdicionales.add(sistema.translate("3.Personal Trainer", "3.Entrenador personal", "3.Treinador pessoal"));
-        serviciosAdicionales.add(sistema.translate("Aulas de Grupo", "Aulas de Grupo", "Aulas de Grupo")); 
+        this.sistema = new SistemaReserva();
+
+        // Agregar servicios adicionales traducidos
+        serviciosAdicionales.add(sistema.translate(
+                "2.Social Events", "2.Eventos Sociales",
+                "2.Eventos Sociais"));
+        serviciosAdicionales.add(sistema.translate(
+                "3.Personal Trainer",
+                "3.Entrenador personal", "3.Treinador pessoal"));
+        serviciosAdicionales.add(sistema.translate(
+                "Aulas de Grupo", "Aulas de Grupo", "Aulas de Grupo"));
     }
-    
 
     /**
      * Muestra la disponibilidad del gimnasio incluyendo horario de apertura,
      * cierre y capacidad.
      */
     @Override
-   public void verDisponibilidadEspacio() {
+    public void verDisponibilidadEspacio() {
         super.verDisponibilidadEspacio(); // Llamar al método de la clase base
-        
+
         // Mostrar mensajes traducidos según el idioma guardado en 'language'
         System.out.println(sistema.translate(
- "Opening hours 6am", "Horario de apertura 6am", "Horário de abertura 6am") + ": " + horarioApertura);
+                "Opening hours 6am"
+                , "Horario de apertura 6am"
+                , "Horário de abertura 6am") + ": " + horarioApertura);
         System.out.println(sistema.translate(
-"Closing hours 10pm", "Horario de cierre 10pm", "Horário de fechamento 10pm") + ": " + horarioCierre);
+                "Closing hours 10pm",
+                "Horario de cierre 10pm",
+                "Horário de fechamento 10pm") + ": " + horarioCierre);
         System.out.println(sistema.translate(
-"Maximum capacity :50 ", "Capacidad máxima :50", "Capacidade máxima :50") + ": " + capacidadMaxima);
+                "Maximum capacity :50 ",
+                "Capacidad máxima :50", 
+                "Capacidade máxima :50") + ": " + capacidadMaxima);
         System.out.println(sistema.translate(
-"Number of machines :20", "Número de máquinas :20", "Número de máquinas:20") + ": " + cantidadMaquinas);
-        
+                "Number of machines :20", 
+                "Número de máquinas :20", 
+                "Número de máquinas:20") + ": " + cantidadMaquinas);
+
         // Mostrar los servicios adicionales traducidos
         System.out.println(sistema.translate(
-"Additional services", "Servicios adicionales", "Serviços adicionais") + ":");
-for (String servicio : serviciosAdicionales) {
+                "Additional services", 
+                "Servicios adicionales", "Serviços adicionais") + ":");
+        for (String servicio : serviciosAdicionales) {
             System.out.println("- " + servicio);
 
         }
-}
+    }
 // Getters y setters
+
     public String getHorarioApertura() {
         return horarioApertura;
     }
@@ -101,5 +117,5 @@ for (String servicio : serviciosAdicionales) {
 
     public void setCantidadMaquinas(int cantidadMaquinas) {
         this.cantidadMaquinas = cantidadMaquinas;
- }
+    }
 }
